968e10fd881b73f746c3a01a16a13cec71f8891b
OanhToNgo <oanhngo94@gmail.com>
2022-06-15 11:55:02 +0700

    refactor the main code and green 4ever always

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+*GitHub Actions to demonstrate the Continuous Intergration (CI) technique
+*Regression Test
 ###Screenshots
 ![DDT & TDD with JUnit](https://github.com/OanhToNgo/math-util/blob/main/images/DDTwithJUnit.jpg)
--- src/com/tina/mathutil/core/MathUtility.java
+++ src/com/tina/mathutil/core/MathUtility.java
@@ -23,15 +23,32 @@ public class MathUtility {
         //20 giai thua 18 con so 0, vua kip du cho kieu long cua Java
         //21 giai thua tran kieu long
         //bai nay quy uoc tinh n! trong khoang tu 0..20
-    public static long getFactorial(int n){
-            if(n <0 || n >20)
-                throw new IllegalArgumentException("Invalid argument.N must be between 0 and 20.");
+//    public static long getFactorial(int n){
+//            if(n <0 || n >20)
+//                throw new IllegalArgumentException("Invalid argument.N must be between 0 and 20.");
+//            if(n == 0 || n ==1 )
+//                return 1; //ket thuc cuoc choi som neu nhan nhung dau vao dac biet
+//            long product = 1;// tich nhan don, thuat toan heo dat, oc buu don thit
+//            for (int i = 2; i <= n; i++) 
+//                product *= i;
+//            return product;    
+//        }
+    //hien tuong goi lai chinh minh voi 1 quy mo khac
+    //vd: matryoshka, giống nhau và lồng trong nhau
+    //bup be to, nho hon, nho hon nua, nho nho hon nua...den diem dung
+    
+    //tinh gium tui 6!
+    //6! = 1.2.3.4.5.6
+    //6! = 5! x 6
+    //1! = 1
+    //n! =1.2.3.4....n
+    //chot ha: n! = n x (n-1)!
+     public static long getFactorial(int n){
+            if(n<0 || n>20)
+                throw new IllegalArgumentException("n must be between 0 to 20");
             if(n == 0 || n ==1 )
                 return 1; //ket thuc cuoc choi som neu nhan nhung dau vao dac biet
-            long product = 1;// tich nhan don, thuat toan heo dat, oc buu don thit
-            for (int i = 2; i <= n; i++) 
-                product *= i;
-            return product;    
+            return n * getFactorial(n-1);    
         }
     
 }
